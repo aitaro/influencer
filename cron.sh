8d893f292d5a54abf9cef27c54a0148715674d5b
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-04-22 17:42:26 +0900

    test5

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PATH=/home/aitaro/.anyenv/envs/pyenv/shims:/home/aitaro/.anyenv/envs/pyenv/bin:/home/aitaro/.anyenv/bin:$PATH
-eval('pipenv install python main.py')
+pipenv run python main.py
