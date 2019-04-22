a8e095400ba9b008eb8128dd8a75f0aeddf9b2aa
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-04-22 17:44:11 +0900

    test5

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PATH=/home/aitaro/.anyenv/envs/pyenv/shims:/home/aitaro/.anyenv/envs/pyenv/bin:/home/aitaro/.anyenv/bin:$PATH
+PATH=/home/aitaro/.anyenv/envs/pyenv/shims:/home/aitaro/.anyenv/envs/pyenv/bin:/home/aitaro/.anyenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 pipenv run python main.py
