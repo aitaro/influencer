ea91b619f598b89ef0aaa1f50107d13c8a001993
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-04-22 17:47:39 +0900

    test5

--- cron.sh
+++ cron.sh
@@ -2,3 +2,4 @@
 PATH=/home/aitaro/.anyenv/envs/pyenv/shims:/home/aitaro/.anyenv/envs/pyenv/bin:/home/aitaro/.anyenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 echo $PATH
 python main.py
+echo 'finish'
