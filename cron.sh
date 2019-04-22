a86218f6c508c74f6821a0e49b7341673ec39553
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-04-22 17:52:23 +0900

    test5

--- cron.sh
+++ cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PATH=/home/aitaro/.anyenv/envs/pyenv/shims:/home/aitaro/.anyenv/envs/pyenv/bin:/home/aitaro/.anyenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 echo $PATH
-pipenv run python main.py
+pipenv run python main.py >> /home/aitaro/influencer/stdout.log
 echo 'finish'
