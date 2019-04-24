d8ee6d1b18898eaf825ef00fde32a3659ff5a961
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-04-24 13:23:01 +0900

    update cron

--- cron.sh
+++ cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PATH=/home/aitaro/.anyenv/envs/pyenv/shims:/home/aitaro/.anyenv/envs/pyenv/bin:/home/aitaro/.anyenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 echo $PATH
+echo date
 pipenv run python main.py >> /home/aitaro/influencer/stdout.log
 echo 'finish'
