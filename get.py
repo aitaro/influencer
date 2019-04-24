a35fa9aeb9e690922cc07515429851f594c26bda
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-04-24 17:36:37 +0900

    add tweet

--- get.py
+++ get.py
@@ -10,20 +10,20 @@ def get_tabelog_url(num):
 
     # 検索
     res = search.getSearchResponse(target_keyword)
-    # レスポンスを格納
 
+    # レスポンスを格納
     f = open(f"post_tweets/{num}/search_tabelog.yml", "w")
     f.write(yaml.dump(res, allow_unicode=True))
 
     print(res['response'][0]['items'][0]['link'])
 
+    # urlをupdate
     t.url = res['response'][0]['items'][0]['link']
     t.save()
     return t.url
 
-
 if __name__ == '__main__':
 
-    get_tabelog_url(15)
-    get_tabelog_url(16)
-    get_tabelog_url(17)
+    # get_tabelog_url(15)
+    # get_tabelog_url(16)
+    get_tabelog_url(18)
