0684fef546b49b5f80f3849af57a4a5494a1c4ac
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-04-28 16:32:22 +0900

    update tweet

--- cron.sh
+++ cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PATH=/home/aitaro/.anyenv/envs/pyenv/shims:/home/aitaro/.anyenv/envs/pyenv/bin:/home/aitaro/.anyenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+SLACK_WEBHOOK_URL='https://hooks.slack.com/services/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'
 echo $PATH
 $(echo date)
 pipenv run python main.py
