4809a04a0027b696a42e95393f030cb135c2ef40
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-05-05 14:14:39 +0200

    add tweet

--- get.py
+++ get.py
@@ -84,7 +84,7 @@ def createTweet2(i):
         # cafe = {'name':'hoge', 'details': 'fuga'}
         # set_trace()
         num += 1
-        # if num <= 104: continue
+        if num <= 121: continue
         t = Tweet(num)
         t.name = cafe['name']
         t.details = cafe['details']
