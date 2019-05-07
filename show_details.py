a54f3efed3f3b225a59653f78049441bf08bdb2b
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-05-07 17:14:44 +0900

    add tweet

---
+++ show_details.py
@@ -0,0 +1,16 @@
+import glob
+import sys
+import os
+import yaml
+import unicodedata
+from tweet import Tweet
+
+num1 = int(sys.argv[1])
+if len(sys.argv) == 3:
+    num2 = int(sys.argv[2])
+else:
+    num2 = num1
+
+for i in range(num1, num2+1):
+    t = Tweet(i)
+    print(t.details)
